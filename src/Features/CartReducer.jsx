0461ca0eb56240abfe1d6cbc49d071bf0a8d4b43
HealedMf <bewajigamer20@gmail.com>
2024-12-26 10:27:38 +0100

    Updated file

--- src/Features/CartReducer.jsx
+++ src/Features/CartReducer.jsx
@@ -1,18 +1,32 @@
+export const totalItem=(cart) => {
+    const total = cart.reduce((sum, product) => sum + product.quantity, 0);
+    return total;
+}
+
+export const totalPrice=(cart) => {
+    const total = cart.reduce((total, product) => total + product.quantity * product.price, 0);
+    return total;
+}
 
 const cartReducer = (state, action) =>{
     switch (action.type) {
         case 'Add':
-            return [...state, action.item];
+            return [...state, action.product];
           
         case 'Remove':
+            return state.filter((item) => item.id !== action.id);
 
         case 'Increase':
-                   
+            const IndexI = state.findIndex(p => p.id === action.id)
+            state[IndexI].quantity += 1
+            return[...state];
+
         case 'Decrease':
+            const IndexD = state.findIndex((item) => item.id === action.id)
+            state[IndexD].quantity -= 1;
+            return[...state];
 
-        default:
-
-        return state;
+        default: return state;
     }
 }
 
--- src/Shop/Data.jsx
+++ src/Shop/Data.jsx
@@ -1,22 +1,21 @@
 import React, { useContext } from 'react'
-import Images from "../img"
 import "../App.css"
 import { Link } from 'react-router-dom'
 import { CartContext } from '../Features/Contextprovider'
 
-const Data = ({products}) => {
+const Data = ({product}) => {
   const {dispatch} = useContext(CartContext)
   return (
     <div>
       <div>
         <div className="item-one">
             <div className="item-image">
-              <img src={Images.img1} alt="Woman in a Sleek Gown" />
+              <img src={product.image} alt="Woman in a Sleek Gown" />
             </div>
             <div className="details">
-              <p className='title'>{products.title}</p>
+              <p className='title'>{product.title}</p>
               <div className="amount">
-                <p className='price'><i>${products.price}</i>${products.discounted_price}</p>
+                <p className='price'><i>${product.price}</i>${product.discounted_price}</p>
                 <div className="stars">
                   <svg xmlns="http://www.w3.org/2000/svg"width="16" height="16" fill="currentColor" class="bi bi-star" viewBox="0 0 16 16" >
                     <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.56.56 0 0 0-.163-.505L1.71 6.745l4.052-.576a.53.53 0 0 0 .393-.288L8 2.223l1.847 3.658a.53.53 0 0 0 .393.288l4.052.575-2.906 2.77a.56.56 0 0 0-.163.506l.694 3.957-3.686-1.894a.5.5 0 0 0-.461 0z"/>
@@ -38,7 +37,7 @@ const Data = ({products}) => {
               <hr />
               <div className="like">
                 <div className="plus">
-                  <Link to='/cart' className='cart text-black text-decoration-none' onClick={() => dispatch({type: "Add"})} lo>Add to Cart</Link>
+                  <Link to='/cart' className='cart text-black text-decoration-none' onClick={() => dispatch({type: "Add", product: product})}>Add to Cart</Link>
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="25" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
                     <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
                   </svg>
