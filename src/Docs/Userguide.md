fc66badc2a9c0fac6e485aa58543666c6c5e34bf
Xevenryan45 <taylorsmith4019@gmail.com>
2024-12-29 22:10:24 +0100

    updated

---
+++ src/Docs/Userguide.md
@@ -0,0 +1,40 @@
+# Madam's Boutique - User Guide
+
+## Table of Contents
+* Getting Started
+* Navigation
+* Shopping Experience
+* Customer Support
+
+## Getting Started
+Welcome to Madam's Boutique! To get started, simply click on the "Shop Now" button
+on the homepage. This will take you to the main shopping page where you can browse
+our collection of stylish and affordable clothing.
+
+## Navigation
+Main Categories
+* Designer Collections
+* Ready-to-Wear
+* New Arrivals
+* Sale Items
+
+## Shopping Experience
+Browsing Products
+1. Select desired category from navigation menu
+2. Click on items to view details:
+    * Fabric information
+
+### Making a Purchase
+1. Add to shopping cart
+2. Review cart contents
+3. Proceed to checkout
+
+## Customer Support
+* Customer Support
+Contact Options
+* Email: mail.icon@gmail.com
+* Phone: +234 9033 457 8765
+
+### Style Guide
+* Fashion blog articles
+* Styling tips
--- src/blog/BlogSingle.jsx
+++ src/blog/BlogSingle.jsx
@@ -1,92 +1,5 @@
-// import React from 'react'
-// import Images from '../config/Images';
-// import blogs from './Blogg';
-// import { useParams } from 'react-router-dom';
-// // import HomeBlog from './components/HomeBlog';
-
-
-// export default function BlogSingle() {
-//   const { id } = useParams(); // Get blog ID from route parameter
-//   const selectedBlog = blogs.find((blog) => blog.id === parseInt(id)); // Find matching blog object
-
-//   if (!selectedBlog) {
-//     return <div>Blog not found!</div>; // Handle missing blog
-//   }
-
-//   return (
-//     <>
-//     <section className='titleBar'>
-//       <div className='text'>
-//       <h1>Blog Single</h1>
-//         <p><b>Home</b>Prdouct<p>Blog Single</p></p>  
-//         </div>
-//      </section>
-
-//      <section className="tps-shpng">
-//         <section className="eight-ps">
-//           <div>
-//             <h4>{selectedBlog.subText}</h4> {/* Display blog title from selectedBlog */}
-//             <p style={{ color: 'black' }}>
-//               {/* Your blog content goes here */}
-//             </p>
-//             <img src={Images.img14} alt="" width="800" height="600" />
-//             <p>Molestiae cupiditate inventore animi, maxime sapiente optio, illo est nemo veritatis repellat sunt doloribus nesciunt! Minima laborum magni reiciendis qui voluptate quisquam voluptatem soluta illo eum ullam incidunt rem assumenda eveniet eaque sequi deleniti tenetur dolore amet fugit perspiciatis ipsa, odit. Nesciunt dolor minima esse vero ut ea, repudiandae suscipit!</p>
-//         </div>
-//         <div>
-//             <input type="text" placeholder='Type a keyword and hit enter' />
-//              <h5>Cartegories</h5>
-//             <ul>
-//                 <li> <p>Bags</p>(12)</li>
-//                 <br />
-//                 <li> <p>Shoes</p>(22)</li>
-//                 <br />
-//                 <li> <p>Dress</p>(37)</li>
-//                 <br />
-//                 <li> <p>Accessories</p>(42)</li>
-//                 <br />
-//                 <li> <p>Makeup</p>(14)</li>
-//                 <br />
-//                 <li> <p>Beauty</p>(140)</li>
-//             </ul>
-//         </div>
-//         </section>
-//       </section>
-
-
-//     {/* <section className='tps-shpng'>
-//     <section className="eight-ps">
-//         <div>
-//             <h4>8 Tips for Shopping</h4>
-//             <p style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia suscipit, quisquam doloremque <br/> distinctio perferendis et doloribus unde architecto optio laboriosam porro adipisci sapiente officiis nemo <br />accusamus ad praesentium? Esse minima nisi et. Dolore perferendis, enim praesentium omnis, iste doloremque <br /> quia officia optio deserunt molestiae voluptates soluta architecto tempora</p>
-//             <img src={Images.img14} alt=""  width='800' height='600'/>
-//             <p>Molestiae cupiditate inventore animi, maxime sapiente optio, illo est nemo veritatis repellat sunt doloribus nesciunt! Minima laborum magni reiciendis qui voluptate quisquam voluptatem soluta illo eum ullam incidunt rem assumenda eveniet eaque sequi deleniti tenetur dolore amet fugit perspiciatis ipsa, odit. Nesciunt dolor minima esse vero ut ea, repudiandae suscipit!</p>
-//         </div>
-//         <div>
-//             <input type="text" placeholder='Type a keyword and hit enter' />
-//              <h5>Cartegories</h5>
-//             <ul>
-//                 <li> <p>Bags</p>(12)</li>
-//                 <br />
-//                 <li> <p>Shoes</p>(22)</li>
-//                 <br />
-//                 <li> <p>Dress</p>(37)</li>
-//                 <br />
-//                 <li> <p>Accessories</p>(42)</li>
-//                 <br />
-//                 <li> <p>Makeup</p>(14)</li>
-//                 <br />
-//                 <li> <p>Beauty</p>(140)</li>
-//             </ul>
-//         </div>
-//     </section>    
-//     </section> */}
-//     </>
-//   )
-// }
-
-
 import React from 'react';
-// import Images from '../config/Images';
+import Images from '../config/Images';
 import { blogs } from './Blogg'; // Import blogs array
 import { useParams } from 'react-router-dom';
 
@@ -109,31 +22,28 @@ export default function BlogSingle() {
 
       <section className="tps-shpng">
         <section className="eight-ps">
-          <div>
-            <h4>{selectedBlog.subText}</h4> {/* Display blog title from selectedBlog */}
+          <div className="blog-post">
+            <h4>{selectedBlog.subText}</h4> 
             <p style={{ color: 'black' }}>
-              {/* Your blog content goes here */}
+              {selectedBlog.text}
             </p>
-            <img src={selectedBlog.pics} alt="" width="800" height="600" /> {/* Use selectedBlog image */}
+            <img src={selectedBlog.pics} alt=""  className='blog-pics' style={{ maxWidth: '100%'}} /> {/* Use selectedBlog image */}
             <p>Molestiae cupiditate inventore animi, maxime sapiente optio, illo est nemo veritatis repellat sunt doloribus nesciunt! Minima laborum magni reiciendis qui voluptate quisquam voluptatem soluta illo eum ullam incidunt rem assumenda eveniet eaque sequi deleniti tenetur dolore amet fugit perspiciatis ipsa, odit. Nesciunt dolor minima esse vero ut ea, repudiandae suscipit!</p>
           </div>
-          <div>
-            <input type="text" placeholder='Type a keyword and hit enter' />
-            <h5>Categories</h5>
-            <ul>
-              <li> <p>Bags</p>(12)</li>
-              <br />
-              <li> <p>Shoes</p>(22)</li>
-              <br />
-              <li> <p>Dress</p>(37)</li>
-              <br />
-              <li> <p>Accessories</p>(42)</li>
-              <br />
-              <li> <p>Makeup</p>(14)</li>
-              <br />
-              <li> <p>Beauty</p>(140)</li>
-            </ul>
-          </div>
+
+          
+          <button className="btn">LIFE</button>
+          <button className="btn">SPORTS</button>
+          <button className='btn'>TECH</button>
+          <button className="btn">TRAVEL</button>
+<br /><br />
+     <div className="comment">
+        <div>
+       <img src={Images.img10} alt="" width='170' height='170' />
+      <h4>Lance Smith</h4>
+      <p style={{color:'black'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, <br /> autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam <br />vero culpa sapiente consectetur similique, inventore eos fugit cupiditate <br /> numquam!</p>
+      </div>
+     </div>
         </section>
       </section>
     </>
